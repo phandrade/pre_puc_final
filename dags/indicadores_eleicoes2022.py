454bf960db14cf7bbc1dc77dfe4d1f84806d6c41
Paulo Andrade <paulotlj@gmail.com>
2022-11-14 17:51:10 -0300

    ajustes

--- dags/indicadores_eleicoes2022.py
+++ dags/indicadores_eleicoes2022.py
@@ -127,7 +127,7 @@ def indicadores_eleicoes2022():
                 'Ec2KeyName': 'paulo_pem',
                 'KeepJobFlowAliveWhenNoSteps': True,
                 'TerminationProtected': False,
-                'Ec2SubnetId': 'subnet-0c0fe84fea4012875'
+                'Ec2SubnetId': 'subnet-09e443d0fbde630c9'
             },
 
             Applications=[{'Name': 'Spark'}],
